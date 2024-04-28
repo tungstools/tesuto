4a1c83f0016df20cc5833b1b9957abb5a2da39fc
t2tc <61874231+t2tc@users.noreply.github.com>
2024-04-28 14:12:05 +0800

    some basic es module support.

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # tesuto
 A TypeScript/JavaScript testing framework that doesn't require 200+ dependencies to run.
 
+> [!WARNING]
+> * This project is still in early development, and is not ready for production use.
+> * This project supports only ES Module but not CommonJS, and requires latest Node.js version.
+
 ## Features
 - Simple and familiar API
 - Out-of-box TypeScript support
 - Assertion and ~~mocking~~ (not yet implemented)
 - Runs parallelly using Workers, or not if you don't wish
 - VSCode integration (working on it)
+
+## License
+
+Public Domain CC-0, or MIT (c) Tung Leen if you perfer.
--- entry.ts
+++ entry.ts
@@ -2,10 +2,13 @@
 
 import { log, error, verbose, ansiFormat, fmt1$ } from "./cli.output";
 import { loadConfig, parseCommandLine, registerFlag } from "./cli";
-import { directoryWalker, findTestFiles } from "./entry.find_file";
+import { findTestFiles } from "./entry.find_file";
 import { loadConfigFile } from "./config";
 import { readFileSync } from "node:fs";
 import * as esbuild from "esbuild";
+
+import path from "node:path";
+import { register as registerModule } from "node:module";
 
 function splash() {
     log("Testing via " + ansiFormat("Tesuto", "cyan", "black", "normal"));
@@ -35,6 +38,16 @@ function registerCli() {
     });
 }
 
+function chroot() {
+    try {
+        process.chdir(globalThis.cmdConfig.root);
+    } catch (e) {
+        error("Invalid root directory.");
+        process.exit(1);
+    }
+    return path.resolve(".");
+}
+
 function runTest() {
 
 }
@@ -47,33 +60,28 @@ async function entry() {
     initGlobalThis();
     registerCli();
 
-    let files = parseCommandLine(process.argv.slice(2));
-    try {
-        process.chdir(globalThis.cmdConfig.root);
-    } catch (e) {
-        error("Invalid root directory.");
-        process.exit(1);
-    }
+    let testParams = parseCommandLine(process.argv.slice(2));
 
     loadConfig(await loadConfigFile());
+    const absRoot = chroot();
 
     splash();
     verbose("Verbose mode enabled, and I'll be more chatty :)");
     verbose(`Root directory set to ${fmt1$(cmdConfig.root)}.`);
 
-    if (files.length == 0) {
+    if (testParams.length == 0) {
         verbose("No files provided by command line, searching for test files...");
-        files = findTestFiles();
+        testParams = findTestFiles();
     } else {
         verbose("Using files provided by command line.");
     }
 
     let contents: { file: string, content: string }[] = [];
-    files.forEach((f) => {
+    testParams.forEach((f) => {
         if (f.endsWith(".ts")) {
             verbose(`Transforming file ${ansiFormat(f, "cyan", "black", "normal")}.`);
             let content = readFileSync(f, "utf-8");
-            contents.push({ file: f, content: esbuild.transformSync(content, { loader: "ts" }).code });
+            contents.push({ file: path.resolve(f), content: esbuild.transformSync(content, { loader: "ts" }).code });
         }
     })
     verbose(`Transformed ${fmt1$(contents.length.toString())} files. Pending to run...`);
